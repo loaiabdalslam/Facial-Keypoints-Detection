4eeab9666644c5af829b912399772219e4bf1851
gabir-yusuf <gabir.yusuf@gmail.com>
2019-12-28 00:45:01 +0200

    update training steps

--- train.py
+++ train.py
@@ -4,10 +4,10 @@ import numpy as np
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
-from models import Net
+from models import *
 
 # instantiate the model
-net = Net()
+net = AlexNet()
 print(net)
 
 
@@ -19,8 +19,8 @@ from data_load import Rescale, RandomCrop, Normalize, ToTensor
 
 # define the data transform using transfroms.Compose([..])
 # Note the order is matter
-data_tranform = transforms.Compose([Rescale(250),
-                                    RandomCrop(224),
+data_tranform = transforms.Compose([Rescale((250, 250)),
+                                    RandomCrop((227, 227)),
                                     Normalize(),
                                     ToTensor()])
 
@@ -31,13 +31,67 @@ transformed_dataset = FacialKeypointsDataset(csv_file='data/training_frames_keyp
 
 print('Number of images: ', len(transformed_dataset))
 
+# iterate through the transformed dataset and print some stats about the first few samples
+for i in range(4):
+    sample = transformed_dataset[i]
+    print(i, sample['image'].size(), sample['keypoints'].size())
 
 # Loadiing the data in batches
 # for windows users, change the num_workers to 0 or you will face some issues with your DataLoader failing
-batch_size = 16
+batch_size = 10
+
 train_loader = DataLoader(transformed_dataset,
-                          batch_size = batch_size,
-                          shuffle = True,
-                          num_workers = 4)
+                          batch_size=batch_size,
+                          shuffle=True,
+                          num_workers=4)
+
+import torch.optim as optim
+
+criterion = nn.SmoothL1Loss()
+optimizer = optim.Adam(net.parameters(), lr=0.001, betas=(0.9, 0.999), eps=1e-08)
 
 
+def train_net(n_epochs):
+    # Model in training mode, dropout is on
+    net.train()
+
+    for epoch in range(n_epochs):
+        running_loss = 0.0
+        # train on batches of data
+        for batch_i, data in enumerate(train_loader):
+            # get the input images and theri crosponding labels
+            images, key_pts = data['image'], data['keypoints']
+            # Flatten pts
+            key_pts = key_pts.view(key_pts.size(0), -1)
+            # Wrap them in a torch Variable
+            images, key_pts = Variable(images), Variable(key_pts)
+            # convert the variables to floats for regression loss
+            key_pts = key_pts.type(torch.FloatTensor)
+            images = images.type(torch.FloatTensor)
+
+            # Forward pass to get the output
+            output_pts = net.forward(images)
+            # Calculate the loss between predicted and target keypoints
+            loss = criterion(output_pts, key_pts)
+            # zero the parameter (wight) gradients
+            optimizer.zero_grad()
+            # backword pass to calculate the weight gradients
+            loss.backword()
+            # update the weights
+            optimizer.step()
+
+            # print the loss statistics
+            running_loss += loss.data[0]
+            if batch_i % 40 == 39: # print every 40 batches
+                print('Epoch: {}, Batch: {}, Avg. Loss: {}'.format(epoch + 1, batch_i + 1, running_loss / 40))
+                running_loss = 0.0
+
+    print('Training Finished')
+
+n_epochs = 50
+train_net(n_epochs)
+
+# saving the model parameters
+model_dir = 'saved_models/'
+model_name = 'keypoints_model_AlexNet_50epochs.pth'
+torch.save(net.state_dict(), model_dir + model_name)
