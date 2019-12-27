c557952efdedaccf5f6acbc10efe4dee4ccdb4c5
gabir-yusuf <gabir.yusuf@gmail.com>
2019-12-27 22:21:43 +0200

    Remove extra comments

--- models.py
+++ models.py
@@ -13,12 +13,6 @@ class Net(nn.Module):
     def __init__(self):
         super(Net, self).__init__()
 
-        ## TODO: Define all the layers of this CNN, the only requirements are:
-        ## 1. This network takes in a square (same width and height), grayscale image as input
-        ## 2. It ends with a linear layer that represents the keypoints
-        ## it's suggested that you make this last layer output 136 values, 2 for each of the 68 keypoint (x, y) pairs
-
-        # As an example, you've been given a convolutional layer, which you may (but don't have to) change:
         # 1 input image channel (grayscale), 32 output channels/feature maps, 5x5 square convolution kernel
         self.conv1 = nn.Conv2d(1, 32, 3)
         self.pool = nn.MaxPool2d(2, 2)
